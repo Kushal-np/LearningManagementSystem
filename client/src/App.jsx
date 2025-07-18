55e2764cc3ce76e45c2afbd07531cad5df2f76e6
Kushal-np <wrckushal@gmail.com>
2025-07-18 12:23:28 +0545

    added the context - first step

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Button } from './components/ui/button'
+import {Route , Routes} from 'react-router-dom'
+import AuthPage from './pages/auth'
 
 const App = () => {
   return (
     <>
-    <Button className="bg-red-300">Check here</Button>
+    <Routes>
+      <Route path="/auth" element={<AuthPage />} />
+    </Routes>
     </>
   )
 }
